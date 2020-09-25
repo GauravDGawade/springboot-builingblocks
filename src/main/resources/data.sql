33560551999807e78f2f8907d8bcace4b38be5d6
Gaurav Gawade <gaurav.gawade@moodys.com>
2020-09-25 11:17:02 -0400

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'gauravgawade@gmail.com', 'Gaurav', 'Gawade', 'admin', 'ssn101', 'gawadeg')
+insert into user values(102, 'sergiobusquest@gmail.com', 'Sergio', 'Busquest', 'admin', 'ssn102', 'sergiob')
+insert into user values(103, 'lionelmessi@gmail.com', 'Lionel', 'Messi', 'admin', 'ssn103', 'messil')
